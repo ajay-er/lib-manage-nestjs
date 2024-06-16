c46fc02a869c433d792bc6400c776cfaa860435d
ajay <ajayzyan100@gmail.com>
2024-06-16 23:30:08 +0530

    refactor(common): add ApiResponse decorator for custom response handling

--- src/common/response/decorators/response.decorator.ts
+++ src/common/response/decorators/response.decorator.ts
@@ -2,7 +2,7 @@ import { applyDecorators, SetMetadata, UseInterceptors } from '@nestjs/common';
 import { ResponseInterceptor } from '@/common/response/interceptors/response.interceptor';
 import { RESPONSE_MESSAGE } from '@/common/response/constants/response.constant';
 
-export function Response(message?: string): MethodDecorator {
+export function ApiResponse(message?: string): MethodDecorator {
   return applyDecorators(
     UseInterceptors(ResponseInterceptor),
     SetMetadata(RESPONSE_MESSAGE, message),

b8f95818f773def3d18c98f60fb1deea1e57e24f
ajay <ajayzyan100@gmail.com>
2024-06-17 00:26:08 +0530

    feat: authors entity with validation included

---
+++ src/modules/authors/repository/entities/authors.entity.ts
@@ -0,0 +1,40 @@
+import { Prop, SchemaFactory } from '@nestjs/mongoose';
+import { DatabaseEntity } from '@/common/database/decorators/database.decorator';
+import { Document } from 'mongoose';
+
+export const AuthorDBCollection = 'author';
+
+@DatabaseEntity({ collection: AuthorDBCollection })
+export class Author extends Document {
+  name: string;
+
+  @Prop({
+    type: String,
+    trim: true,
+    minlength: 0,
+    maxlength: 1000,
+  })
+    biography?: string;
+
+  @Prop({
+    required: true,
+    type: Date,
+    validate: {
+      validator: (value: Date) => {
+        const currentDate = new Date();
+        const tenYearsAgo = new Date(
+          currentDate.getFullYear() - 10,
+          currentDate.getMonth(),
+          currentDate.getDate(),
+        );
+        return value <= tenYearsAgo;
+      },
+      message: 'Birthdate must be at least 10 years ago',
+    },
+  })
+    birthDate: Date;
+}
+
+export const AuthorSchema = SchemaFactory.createForClass(Author);
+
+export type AuthorDoc = Author;

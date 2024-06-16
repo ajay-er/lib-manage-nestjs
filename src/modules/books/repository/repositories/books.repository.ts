29b2b1df5d02a4b3a14f83c43c8fe4500faaa8a7
ajay <ajayzyan100@gmail.com>
2024-06-17 02:48:58 +0530

    feat: book create find findone apis completed

--- src/modules/books/repository/repositories/books.repository.ts
+++ src/modules/books/repository/repositories/books.repository.ts
@@ -3,6 +3,7 @@ import { Model } from 'mongoose';
 import type { BookDoc } from '../entities/books.entity';
 import { Book } from '../entities/books.entity';
 import { DatabaseModel } from '@/common/database/decorators/database.decorator';
+import type { BookDto } from '@/modules/books/dto/book.dto';
 
 @Injectable()
 export class BookRepository {
@@ -10,7 +11,16 @@ export class BookRepository {
     @DatabaseModel(Book.name) private readonly bookModel: Model<Book>,
   ) {}
 
-  async findAll(): Promise<BookDoc[]> {
-    return this.bookModel.find().exec();
+  async create(bookDto: BookDto): Promise<BookDoc> {
+    return this.bookModel.create(bookDto);
+  }
+
+  async findAll(page: number, limit: number): Promise<BookDoc[]> {
+    const skip = (page - 1) * limit;
+    return this.bookModel.find().skip(skip).limit(limit).exec();
+  }
+
+  async findById(id: string): Promise<BookDoc> {
+    return this.bookModel.findById(id);
   }
 }

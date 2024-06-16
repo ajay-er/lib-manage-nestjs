897c30f005f7ede19e7a577b01152a4c86a43c9f
ajay <ajayzyan100@gmail.com>
2024-06-16 23:48:31 +0530

    feat: books module database connection success

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -6,6 +6,7 @@ import { BooksRepositoryModule } from './repository/books.repository.module';
 @Module({
   controllers: [BooksController],
   providers: [BooksService],
+  exports: [BooksService],
   imports: [BooksRepositoryModule],
 })
 export class BooksModule {}

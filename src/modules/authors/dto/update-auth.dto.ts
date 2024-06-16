d80719cfd7095e16b8910ad5bc20e94c244028d1
ajay <ajayzyan100@gmail.com>
2024-06-17 04:27:08 +0530

    feat: authors DTO added

---
+++ src/modules/authors/dto/update-auth.dto.ts
@@ -0,0 +1,45 @@
+/* eslint-disable max-classes-per-file */
+import { Type } from 'class-transformer';
+import type { ValidatorConstraintInterface, ValidationArguments } from 'class-validator';
+import {
+  IsNotEmpty, IsString, Length, IsOptional, IsDate,
+  ValidatorConstraint,
+  Validate,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'IsAtLeastTenYearsOld', async: false })
+class IsAtLeastTenYearsOld implements ValidatorConstraintInterface {
+  validate(date: Date, _args: ValidationArguments) {
+    const currentDate = new Date();
+    const tenYearsAgo = new Date(
+      currentDate.getFullYear() - 10,
+      currentDate.getMonth(),
+      currentDate.getDate(),
+    );
+    return date <= tenYearsAgo;
+  }
+
+  defaultMessage(_args: ValidationArguments) {
+    return 'Birthdate must be at least 10 years ago';
+  }
+}
+
+export class UpdateAuthorDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  @Length(1, 60)
+    name?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(3, 300)
+    biography?: string;
+
+  @IsNotEmpty()
+  @IsOptional()
+  @Type(() => Date)
+  @IsDate()
+  @Validate(IsAtLeastTenYearsOld)
+    birthDate?: Date;
+}

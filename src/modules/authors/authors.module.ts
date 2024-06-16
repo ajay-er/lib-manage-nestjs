217b42917dd78ff0ad20f6874f5aacbec9f66f1a
ajay <ajayzyan100@gmail.com>
2024-06-17 00:50:33 +0530

    feat: authors database base API created

--- src/modules/authors/authors.module.ts
+++ src/modules/authors/authors.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthorsRepositoryModule } from '@/modules/authors/repository/authors.repository.module';
+import { AuthorsService } from '@/modules/authors/services/authors.service';
+import { AuthorsController } from '@/modules/authors/controllers/authors.controller';
 
-@Module({})
+@Module({
+  imports: [AuthorsRepositoryModule],
+  controllers: [AuthorsController],
+  providers: [AuthorsService],
+  exports: [AuthorsService],
+})
 export class AuthorsModule {}
--- src/router.module.ts
+++ src/router.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from '@/modules/health/health.module';
 import { BooksModule } from '@/modules/books/books.module';
+import { AuthorsModule } from '@/modules/authors/authors.module';
 
 @Module({
-  imports: [HealthModule, BooksModule],
+  imports: [HealthModule, BooksModule, AuthorsModule],
 })
 export class RouterModule {}

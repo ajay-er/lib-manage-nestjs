3df5052f993d76abec5f3d5ce15f014347722fc0
ajay <ajayzyan100@gmail.com>
2024-06-17 08:27:19 +0530

    feat(docker): add Dockerfile for containerization

--- src/swagger.ts
+++ src/swagger.ts
@@ -3,11 +3,10 @@ import { ConfigService } from '@nestjs/config';
 import type { NestApplication } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { writeFileSync } from 'fs';
-import { EnumAppEnvironment } from '@/common/constants/app.enum.constant';
+import { resolve } from 'path';
 
 export const swaggerApp = async (app: NestApplication) => {
   const configService = app.get(ConfigService);
-  const env: string = configService.get<string>('app.env');
   const appName: string = configService.get<string>('app.name');
 
   const logger = new Logger();
@@ -16,49 +15,48 @@ export const swaggerApp = async (app: NestApplication) => {
   const docDescription = 'My API docs- lib-manage API';
   const docVersion = '1.0';
 
-  if (env !== EnumAppEnvironment.PRODUCTION) {
-    const documentBuild = new DocumentBuilder()
-      .setTitle(appName)
-      .setDescription(docDescription)
-      .setVersion(docVersion)
-      .addServer('/')
-      .addBearerAuth(
-        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-        'accessToken',
-      )
-      .addBearerAuth(
-        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-        'refreshToken',
-      )
-      .addApiKey({ type: 'apiKey', in: 'header', name: 'x-api-key' }, 'xApiKey')
-      .build();
+  const documentBuild = new DocumentBuilder()
+    .setTitle(appName)
+    .setDescription(docDescription)
+    .setVersion(docVersion)
+    .addServer('/')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'accessToken',
+    )
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'refreshToken',
+    )
+    .build();
 
-    const document = SwaggerModule.createDocument(app, documentBuild, {
-      deepScanRoutes: true,
-    });
+  const document = SwaggerModule.createDocument(app, documentBuild, {
+    deepScanRoutes: true,
+  });
 
-    writeFileSync('swagger.json', JSON.stringify(document));
-    SwaggerModule.setup(docPrefix, app, document, {
-      jsonDocumentUrl: `${docPrefix}/json`,
-      yamlDocumentUrl: `${docPrefix}/yaml`,
-      explorer: true,
-      customSiteTitle: appName,
-      swaggerOptions: {
-        docExpansion: 'none',
-        persistAuthorization: true,
-        displayOperationId: true,
-        operationsSorter: 'method',
-        tagsSorter: 'alpha',
-        tryItOutEnabled: true,
-        filter: true,
-        deepLinking: true,
-      },
-    });
+  const filePath = resolve(__dirname, 'swagger.json');
+  writeFileSync(filePath, JSON.stringify(document));
 
-    logger.log('==========================================================');
+  SwaggerModule.setup(docPrefix, app, document, {
+    jsonDocumentUrl: `${docPrefix}/json`,
+    yamlDocumentUrl: `${docPrefix}/yaml`,
+    explorer: true,
+    customSiteTitle: appName,
+    swaggerOptions: {
+      docExpansion: 'none',
+      persistAuthorization: true,
+      displayOperationId: true,
+      operationsSorter: 'method',
+      tagsSorter: 'alpha',
+      tryItOutEnabled: true,
+      filter: true,
+      deepLinking: true,
+    },
+  });
 
-    logger.log(`Docs will serve on ${docPrefix}`, 'NestApplication');
+  logger.log('==========================================================');
 
-    logger.log('==========================================================');
-  }
+  logger.log(`Docs will serve on ${docPrefix}`, 'NestApplication');
+
+  logger.log('==========================================================');
 };
